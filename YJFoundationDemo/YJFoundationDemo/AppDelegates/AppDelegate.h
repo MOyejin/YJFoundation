b92439baa0f0e965587cbcfac5873f50f3434159
moyejin <moyejin@estronger.cn>
2019-05-10 17:46:44 +0800

    初始化项目

---
+++ YJFoundationDemo/YJFoundationDemo/AppDelegates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJFoundationDemo
+//
+//  Created by EStrongerE023 on 2019/5/10.
+//  Copyright © 2019 EStrongerE023. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJFoundationDemo/YJFoundationDemo/Resource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YJFoundationDemo
+//
+//  Created by EStrongerE023 on 2019/5/10.
+//  Copyright © 2019 EStrongerE023. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
